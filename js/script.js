dddf691dd74450df4406c465b5fff82acc12179e
AbtahiHasan <anikidpersonal@gmail.com>
2023-03-01 01:50:12 +0600

    done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-let cardCount = 15;
+let cardCount = 10;
 
 const catchData = async () => {
-    const url = "https://forbes400.onrender.com/api/forbes400/getAllBillionaires";
+    const url = `https://forbes400.onrender.com/api/forbes400?limit=${cardCount}`;
     const response = await fetch(url);
     const billionaires = await response.json();
     printData(billionaires);   
@@ -9,16 +9,15 @@ const catchData = async () => {
 }
 
 const printData = (billionaires) => {
-    let billionairesArr = billionaires.slice(0, cardCount);
     const cards = document.getElementById("cards");
-    billionairesArr.forEach(billionaire => {
+    billionaires.forEach(billionaire => {
         console.log(billionaire)
             cards.innerHTML += `
             <article class="bg-base-100 p-3 rounded shadow-xl">
-            <h2 class="text-2xl text-center my-3">Name</h2>
+            <h2 class="text-2xl text-center my-3">${billionaire.person.name}</h2>
             <div class="card card-side h-[300px]">
                 <figure>
-                <img class="w-[100%] h-full" src="./images/img.jpg" alt="Movie" />
+                <img class="w-[100%] h-full" src="${billionaire?.person?.squareImage}" alt="Movie" />
                 </figure>
                 <div class="card-body">
                 <h2 class="card-title">New movie is released!</h2>
@@ -30,14 +29,13 @@ const printData = (billionaires) => {
             `
     });
     
-    
 }
 window.addEventListener("load", catchData);
 
 const showMoreBtn = document.getElementById("show-more-btn");
 
 showMoreBtn.onclick = () => {
-    cardCount += 5;
+    cardCount += 3;
     catchData();
 }
 
